1bd592b73c0f38203ab98fa2d7028386443e53ef
jeffnotjeffery <jeffery.hubbart@yahoo.com>
2024-04-29 22:38:26 -0500

    sa18-act2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: 'Oven Mitts', price: 10.99, description: 'Have you ever been baking and had to grab something out of the oven? Thats what we are for! Give use a try and you will not regret it!')
+Product.create(name: 'Kitchen Dish Towels', price: 15.99, description: 'Have you ever needed to dry your hands while cleaning the kitchen? Thats what we are for! Give us a try and you wont try anyone else')
+Product.create(name: 'Toaster', price: 62.99, description: 'Imagine wanting cooked bread for breakfast and having to warm up your oven. Fear not, the toaster wil save your day!')
